f2318291765bd17d2f8837b098be4061f3c53751
Jorge Araya Navarro <jorge@esavara.cr>
2018-09-13 19:33:39 -0600

    new: Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='rs',
+    version='0.1',
+    description='"To be written"',
+    url='https://github.com/jdpc91/redabitsScabcr',
+    author='RedaBits',
+    author_email='redabits@gmail.com',
+    license='Propietary',
+    packages=['rs'],
+    install_requires=[],
+    scripts=[],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    zip_safe=False)
