6cccc342bdead7d9f4a15b5ed49f73d0cf179383
Jorge Araya Navarro <jorge@esavara.cr>
2018-09-14 02:19:22 -0600

    new: Provide object for connection with MSSQL database
    `DATABASE_URL` is an environment variable which holds the information required to connect with a
    database server.

--- rs/__init__.py
+++ rs/__init__.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from os import getenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DEV_DATABASE_URL = ("mssql+pyodbc://sa:u2ykHVe3XMSPzvL9@127.0.0.1:1433/"
+                    "TESTING?driver=ODBC+Driver+17+for+SQL+Server")
+
+engine = create_engine(getenv('DATABASE_URL', DEV_DATABASE_URL), echo=False)
+# You import `session` for doing calls to the database, more about the session
+# object here:
+# https://docs.sqlalchemy.org/en/latest/orm/session_api.html#session-and-sessionmaker
+session = sessionmaker(bind=engine)()
