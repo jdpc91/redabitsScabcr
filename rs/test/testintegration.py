2ad5864fd2f0712ad0ff130561c67a3ab5b791fa
Jorge Araya Navarro <jorge@esavara.cr>
2018-09-15 23:22:59 -0600

    chg: Update integration test

--- rs/test/testintegration.py
+++ rs/test/testintegration.py
@@ -4,6 +4,7 @@ import os
 import unittest
 from datetime import datetime
 
+from rs import session
 from rs.models import Factura
 
 
@@ -13,12 +14,16 @@ class TestIntegration(unittest.TestCase):
         os.environ['DEBUG'] = "ON"
 
         # Create some invoices, this should trigger the SP and make new
-        # receipts
+        # receipts. WARNING: There is no rollback between tests, so changes in
+        # the tables are accumulative, thus, it may take disk space or add
+        # trash to production database if used for testing.
         Factura()._populate()
+
+    def tearDown(self):
+        session.rollback()
 
     def test_send(self):
         """ Send a recipe and see what happens
         """
         from rs.funcs import sendall
-        r = sendall()
-        assert r
+        sendall()
